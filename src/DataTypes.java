d290eec0b0b64478f22d0f5f871558336ec3eb09
Magpie512 <224426406+Magpie512@users.noreply.github.com>
2026-01-08 18:42:47 -0500

    Add DataTypes class with main method and placeholder comments for data types

---
+++ src/DataTypes.java
@@ -0,0 +1,17 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        /*
+        String
+        double
+        floar
+        int
+        short
+        long
+        boolean
+        byte
+        Object
+        Char
+        */
+
+    }
+}
