6af66e1faf38c6fb1ec10868fe490c12e95e3b6e
Magpie512 <224426406+Magpie512@users.noreply.github.com>
2026-01-08 18:50:40 -0500

    Add MathOperationsWithUserInput class for basic arithmetic operations with user input

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -13,5 +13,16 @@ public class DataTypes {
         Char
         */
 
+        int age = 25;
+        char c += 'A';
+        doubled double = 3D; // Literal character d/D, f/F, l/L;
+        float f = 30000f;
+        String name = "John Doe";
+        boolean isActive = true;
+        byte b1 = (byte)10; //literal values: whole numbers between -128 to 127, decimal numbers between -128.0 to 127.0, byte nunbers between -128b to 127b
+        byte b2 = (byte)20;
+        short s = (short) (b1 + b2);
+        long l = 123456789L;
+
     }
 }
